0f422fe608a08f79c779429df3d8a94278df5418
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-06-26 18:51:30 +0500

    added angular UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatSelectChange} from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crypto';
+  selectedCurrency = 'inr';
+
+  constructor() {
+  }
+
+  sendCurrency(val: MatSelectChange) {
+    console.log(val)
+  }
 }
