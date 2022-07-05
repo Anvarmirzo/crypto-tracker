83fa1a9f58e282d2711e97c05d46cb32565542cc
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-07-05 17:09:56 +0500

    build folder renamed (#8)

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/crypto-tracker'));
+app.use(express.static(__dirname + '/dist/crypto'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/crypto-tracker/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/crypto/index.html'));
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Task Tracker is listening on port ${port}...`));
